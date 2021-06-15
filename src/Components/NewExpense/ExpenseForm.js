6113329f1d118d239cb3bf72c86f7d82a70a6949
Irehmanullah <irehmanullah@gmail.com>
2021-06-15 11:59:42 +0500

    changes

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -10,21 +10,33 @@ const ExpenseForm = () => {
         enteredDate: ''
     });
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value,
+        // setEnteredTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredTitle: event.target.value };
         });
     };
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value,
+        // setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredAmount: event.target.value };
         });
     };
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value,
+        // setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value,
+        // });
+        setUserInput((prevState) => {
+            return { ...prevState, enteredDate: event.target.value };
         });
     };
     return (
