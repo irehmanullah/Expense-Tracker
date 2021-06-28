31d827a6e448bc026309656de8187f79ee10099a
irehmanullah <irehmanullah@gmail.com>
2021-06-28 23:40:58 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -17,26 +17,7 @@ function App() {
       date: new Date(2020, 12, 3)
     },
   ]
-  const [expenses, setExpenses] = useState([]);
-  console.log(expenses)
-  const saveLocalTodos = () => {
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-  }
-  const getLocalTodos = () => {
-    if (localStorage.getItem("expenses") === null) {
-      localStorage.setItem("expenses", JSON.stringify([]));
-    } else {
-      let expensesLocal = JSON.parse(localStorage.getItem("expenses"));
-      setExpenses(expensesLocal);
-    }
-  }
-  useEffect(() => {
-    // addExpenseHandler();
-    getLocalTodos();
-  }, [])
-  useEffect(() => {
-    saveLocalTodos();
-  }, [expenses])
+  const [expenses, setExpenses] = useState(dummyExpenses);
   const addExpenseHandler = (expense) => {
     setExpenses(prevState => {
       return [expense, ...prevState];
